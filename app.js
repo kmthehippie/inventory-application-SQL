9eff04e991d9e56ab4503c254ed652e458eb306b
kmthehippie <66264446+kmthehippie@users.noreply.github.com>
2024-09-26 01:01:53 +0800

    Update to listen on 0.0.0.0

--- app.js
+++ app.js
@@ -48,4 +48,6 @@ app.use(function (err, req, res, next) {
   res.render("error", { title: "Error", error: err });
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, "0.0.0.0", () => {
+  console.log(`Listening on 0.0.0.0 ${process.env.PORT}`);
+});
